90721cdbbc77c5d6e05805963832fd9844916b41
Tom Doughty <thomashdoughty@gmail.com>
2021-10-13 14:26:14 +0100

    Add error logging

--- functions/login.js
+++ functions/login.js
@@ -4,18 +4,28 @@ const crypto = require('crypto');
 const { spotifyApi } = require('./utils');
 
 exports.handler = () => {
-  // Create state so it can be used to ensure same browser logs in as creates original request
-  const state = crypto.randomBytes(20).toString('hex');
-  const url = spotifyApi.createAuthorizeURL(['user-read-private', 'user-read-email'], state);
-  console.log(url)
+  try {
+    // Create state so it can be used to ensure same browser logs in as creates original request
+    const state = crypto.randomBytes(20).toString('hex');
+    const url = spotifyApi.createAuthorizeURL(['user-read-private', 'user-read-email'], state);
+    console.log(url)
 
-  // Redirect to Spotify auth and set state cookie
-  return {
-    statusCode: 302,
-      headers: {
-         'Location': url,
-         'Access-Control-Expose-Headers': 'Set-Cookie',
-         'Set-Cookie': `state=${state}; Path=/; Max-Age=60`,
-      }
+    // Redirect to Spotify auth and set state cookie
+    return {
+      statusCode: 302,
+        headers: {
+          'Location': url,
+          'Access-Control-Expose-Headers': 'Set-Cookie',
+          'Set-Cookie': `state=${state}; Path=/; Max-Age=60`,
+        }
+    }
+  } catch(error) {
+    // TODO create error route within application
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: error.message
+      }),
+    }
   }
 };
