94eb2fb15f6240f7b05df3de7292a5af517c7212
Tom Doughty <thomashdoughty@gmail.com>
2021-10-13 14:17:51 +0100

    Update login function

--- functions/login.js
+++ functions/login.js
@@ -6,12 +6,14 @@ const { spotifyApi } = require('./utils');
 exports.handler = () => {
   // Create state so it can be used to ensure same browser logs in as creates original request
   const state = crypto.randomBytes(20).toString('hex');
+  const url = spotifyApi.createAuthorizeURL(['user-read-private', 'user-read-email'], state);
+  console.log(url)
 
   // Redirect to Spotify auth and set state cookie
   return {
     statusCode: 302,
       headers: {
-         'Location': spotifyApi.createAuthorizeURL(['user-read-private', 'user-read-email'], state),
+         'Location': url,
          'Access-Control-Expose-Headers': 'Set-Cookie',
          'Set-Cookie': `state=${state}; Path=/; Max-Age=60`,
       }
